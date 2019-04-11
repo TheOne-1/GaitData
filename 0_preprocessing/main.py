3499757d3d3c8025fa51ecb5aaf7ce5309509f54
TheOne-1 <alan.tan.tian@outlook.com>
2019-04-11 17:11:34 +0800

    version 0.0.1

--- 0_preprocessing/main.py
+++ 0_preprocessing/main.py
@@ -8,13 +8,9 @@ file_names_marker = ['static 2.csv', 'mini 24 strike index 3.csv']
 
 file_path_haisheng = FOLDER_PATH + 'HaishengSensors\\' + file_names_haisheng[1]
 my_haisheng_sensor_reader = HaishengSensorReader(file_path_haisheng)
-gyr_norm_haisheng = my_haisheng_sensor_reader.get_gyr_norm()
 
 file_path_marker = FOLDER_PATH + 'Vicon\\' + file_names_marker[0]
 my_vicon_reader = ViconReader(file_path_marker)
-offsets = my_vicon_reader._get_plate_calibration()
+plate_data_df = my_vicon_reader.get_plate_processed()
 segment_marker_df = my_vicon_reader.get_marker_data_processed_segment('r_foot')
 data_marker_segment_mat = segment_marker_df.values
-
-plt.plot(gyr_norm_haisheng)
-plt.show()
--- 0_preprocessing/preprocessor.py
+++ 0_preprocessing/preprocessor.py
@@ -11,22 +11,30 @@ import matplotlib.pyplot as plt
 class ViconReader:
     def __init__(self, file):
         self._file = file
-        self.marker_offset = self._find_marker_start_row()  # get the offset
+        self.marker_start_row = self._find_marker_start_row()  # get the offset
         self.marker_data_all_df = self.__get_marker_processed()
-        # self.plate_data = self.__get_plate_processed()
 
     def _find_marker_start_row(self):
+        """
+        For the csv file exported by Vicon Nexus, this function find the start row of the marker
+        :return: marker start row
+        """
         with open(self._file, 'r') as csvfile:
             reader = csv.reader(csvfile)
             i_row = 0
             for row in reader:
                 if row and (row[0] == 'Trajectories'):
-                    marker_offset = i_row + 3
+                    marker_start_row = i_row + 3
                     break
                 i_row += 1
-        return marker_offset
+        return marker_start_row
 
-    def get_marker_names(self, row_num):
+    def _get_marker_names(self, row_num):
+        """
+        This function automatically find all the marker names
+        :param row_num: the row number of marker
+        :return:
+        """
         with open(self._file, 'r', errors='ignore') as csvfile:
             reader = csv.reader(csvfile)
             # get the row of names
@@ -47,7 +55,7 @@ class ViconReader:
 
     def _get_marker_raw(self):
         # skip the force data and a couple rows when selecting marker data
-        skip_range = list(range(0, self.marker_offset + 2))
+        skip_range = list(range(0, self.marker_start_row + 2))
         data_raw_marker = pd.read_csv(self._file, skiprows=skip_range, header=None)
         return data_raw_marker
 
@@ -67,7 +75,7 @@ class ViconReader:
         data_marker = filtfilt(b_marker, a_marker, data_marker, axis=0)  # filtering
         data_marker = np.column_stack([data_raw_marker[:, 0], data_marker])
 
-        column_names = self.get_marker_names(self.marker_offset-1)
+        column_names = self._get_marker_names(self.marker_start_row - 1)
         data_marker_df = pd.DataFrame(data_marker, columns=column_names)
         return data_marker_df
 
@@ -87,68 +95,77 @@ class ViconReader:
         return self.marker_data_all_df[marker_name_xyz]
 
     @staticmethod
-    def _get_plate_calibration(plate_1_file='plate1.csv', plate_2_file='plate2.csv'):
+    def _get_plate_calibration():
         # needs debugging !!!
-        cop_offsets = []
-        for plate_file in [plate_1_file, plate_2_file]:
+        cop_offsets_list = []
+        for plate_file in ['plate1.csv', 'plate2.csv']:
             plate_reader = ViconReader(FOLDER_PATH + 'Vicon\\' + plate_file)
             data_DL = plate_reader.get_marker_data_one_marker('DL').values
             data_DR = plate_reader.get_marker_data_one_marker('DR').values
             data_ML = plate_reader.get_marker_data_one_marker('ML').values
             center_vicon = (data_DL + data_DR) / 2 + (data_DL - data_ML)
-            plate_data_raw = plate_reader._get_plate_raw()
+            plate_data_raw = plate_reader._get_plate_data_raw_resampled()
             if '1' in plate_file:
                 center_plate = plate_data_raw[['Cx', 'Cy', 'Cz']].values
             elif '2' in plate_file:
                 center_plate = plate_data_raw[['Cx.1', 'Cy.1', 'Cz.1']].values
             else:
                 raise RuntimeError('Plate 1 or 2 not specified')
-            cop_offsets.append(np.mean(center_plate - center_vicon, axis=0))
-        return np.concatenate([cop_offsets[0], cop_offsets[1]])
+            cop_offsets_list.append(np.mean(center_plate - center_vicon, axis=0))
+        cop_offsets = np.concatenate([cop_offsets_list[0], cop_offsets_list[1]])
+        cop_offsets += np.array([279.4, 784, 0, 842.1, 784, 0])     # coordinate difference between
+        return cop_offsets
 
     def _get_plate_raw(self):
-
-        plate_data_raw = pd.read_csv(self._file, skiprows=[0, 1, 2, 4], nrows=self.marker_offset - 9).astype(float)
+        plate_data_raw = pd.read_csv(self._file, skiprows=[0, 1, 2, 4], nrows=self.marker_start_row - 9).astype(float)
         # only keep useful columns
         plate_data_raw = plate_data_raw[['Frame', 'Fx', 'Fy', 'Fz', 'Cx', 'Cy', 'Cz', 'Fx.1', 'Fy.1', 'Fz.1', 'Cx.1', 'Cy.1', 'Cz.1']]
         return plate_data_raw
 
-    def __get_plate_processed(self, wn_plate=50 / (PLATE_SAMPLE_RATE / 2), filter_order=4):
+    def get_plate_processed(self, cut_off_fre=50, filter_order=4):
+        """
+        Process include COP calibration and filtering.
+        :param cut_off_fre: cut off frequency of the butterworth low pass filter
+        :param filter_order: butterworth filter order
+        :return: plate_data_df
+        """
         plate_data_raw = self._get_plate_raw().values
+        # calibrate COP differences between force plate and vicon
         plate_offsets = self._get_plate_calibration()
-        for channel in range(4, 7):
+        for channel in range(4, 7):     # Minus the COP offset of the first plate
             plate_data_raw[:, channel] -= plate_offsets[channel-4]
-        for channel in range(10, 13):
+        for channel in range(10, 13):   # Minus the COP offset of the second plate
             plate_data_raw[:, channel] -= plate_offsets[channel-7]
 
-        # set threshold to the plate force, 先设阈值再滤波
-        threshold = 50
-        l_total_force = norm(plate_data_raw[:, 1:4], axis=1)
-        r_total_force = norm(plate_data_raw[:, 7:10], axis=1)
         # filter the force data
         plate_data = plate_data_raw[:, 1:]
+        wn_plate = cut_off_fre / (PLATE_SAMPLE_RATE / 2)
         b_force_plate, a_force_plate = butter(filter_order, wn_plate, btype='low')
-        plate_data = filtfilt(b_force_plate, a_force_plate, plate_data, axis=0)  # filtering
-        plate_data = np.column_stack((plate_data_raw[:, 0], plate_data))  # stack the time sample
-        i_row = 0
-        for l_force in l_total_force:
-            if l_force < threshold:
-                plate_data[i_row, 1:4] = 0
-            i_row += 1
-        i_row = 0
-        for r_force in r_total_force:
-            if r_force < threshold:
-                plate_data[i_row, 7:10] = 0
-            i_row += 1
-        plate_data_df = pd.DataFrame(plate_data, columns=FORCE_NAMES)
+        plate_data_filtered = filtfilt(b_force_plate, a_force_plate, plate_data, axis=0)  # filtering
+        plate_data_filtered = np.column_stack((plate_data_raw[:, 0], plate_data_filtered))  # stack the time sample
+        plate_data_df = pd.DataFrame(plate_data_filtered, columns=FORCE_NAMES)
         return plate_data_df
+
+    def _get_plate_data_raw_resampled(self, resample_fre=MOCAP_SAMPLE_RATE):
+        """
+        The returned data is unfiltered.
+        :param resample_fre:
+        :return:
+        """
+        plate_data_df = self._get_plate_raw()
+        ratio = PLATE_SAMPLE_RATE / resample_fre
+        if ratio - int(ratio) > 1e-6:    # check if ratio is an int
+            raise RuntimeError('resample failed, try interpolation')
+        data_len = plate_data_df.shape[0]
+        force_data_range = range(0, data_len, int(ratio))
+        return plate_data_df.loc[force_data_range]
 
     def get_plate_data_resampled(self, resample_fre=MOCAP_SAMPLE_RATE):
         """
         No interpolation included
         :return:
         """
-        plate_data_df = self.__get_plate_processed()
+        plate_data_df = self.get_plate_processed()
         ratio = PLATE_SAMPLE_RATE / resample_fre
         if ratio - int(ratio) > 1e-6:    # check if ratio is an int
             raise RuntimeError('resample failed, try interpolation')
@@ -160,14 +177,31 @@ class ViconReader:
 class HaishengSensorReader:
     def __init__(self, file):
         self._file = file
+        self.data_processed_df = self.__get_sensor_data_processed()
 
     def _get_sensor_data_raw(self):
         data_raw_haisheng = pd.read_csv(self._file)
         data_raw_haisheng.columns = COLUMN_NAMES_HAISHENG
         return data_raw_haisheng
 
-    def get_sensor_data_processed(self, wn=20/(HAISHENG_SENSOR_SAMPLE_RATE/2), filter_order=4):
+    def _get_channel_data_raw(self, channel):
         """
+        :param channel: Channel can be a str or a list.
+        For str, acceptable names are: 'acc_x', 'acc_y', 'acc_z', 'gyr_x', 'gyr_y', 'gyr_z', 'mag_x', 'mag_y', 'mag_z'
+        For list, each element should be one of the str specified above.
+        :return:
+        """
+        data_raw = self._get_sensor_data_raw()
+        if isinstance(channel, list):
+            return data_raw[channel]
+        elif isinstance(channel, str):
+            return data_raw[[channel]]
+        else:
+            raise ValueError('Wrong channel type')
+
+    def __get_sensor_data_processed(self, wn=20 / (HAISHENG_SENSOR_SAMPLE_RATE / 2), filter_order=4):
+        """
+        This function is invoked during initialization. Please use self.data_processed_df to get data
         process include filtering
         :param wn: normalized frequency
         :param filter_order: butterworth filter order
@@ -180,34 +214,38 @@ class HaishengSensorReader:
         data_haisheng_df = pd.DataFrame(data_haisheng, columns=COLUMN_NAMES_HAISHENG[4:10])
         return data_haisheng_df
 
-    def get_processed_channel_data(self, channel):
-        data_haisheng_df = self.get_sensor_data_processed()
+    def get_sensor_data_processed(self):
+        return self.data_processed_df
+
+    def get_channel_data_processed(self, channel):
+        """
+        :param channel: Channel can be a str or a list.
+        For str, acceptable names are: 'acc_x', 'acc_y', 'acc_z', 'gyr_x', 'gyr_y', 'gyr_z', 'mag_x', 'mag_y', 'mag_z'
+        For list, each element should be one of the str specified above.
+        :return:
+        """
         if isinstance(channel, list):
-            return data_haisheng_df[channel].values
+            return self.data_processed_df[channel]
         elif isinstance(channel, str):
-            return data_haisheng_df[[channel]].values.reshape(-1)
+            return self.data_processed_df[[channel]]
         else:
-            raise RuntimeError('Wrong channel type')
+            raise ValueError('Wrong channel type')
 
-    def get_gyr_norm(self):
-        data_haisheng_df = self.get_sensor_data_processed()
-        gyr_data = data_haisheng_df[['gyr_x', 'gyr_y', 'gyr_z']]
-        return norm(gyr_data, axis=1)
 
-    @staticmethod
-    def sync_with_vicon(vicon_gyr, sensor_gyr, check=False):
-        correlation = np.correlate(vicon_gyr, sensor_gyr, 'valid')
-        vicon_data_len = len(vicon_gyr)
-        sensor_data_len = len(sensor_gyr)
-        # normalize correlation data
-        for i_sample in range(sensor_data_len - vicon_data_len + 1):
-            norm_range = range(i_sample, i_sample+vicon_data_len)
-            correlation[i_sample] = correlation[i_sample] / norm(sensor_gyr[norm_range])
-
-        if check:
-            plt.plot(correlation)
-            print(np.argmax(correlation) + vicon_data_len)
-            # plt.show()
-        diff = len(sensor_gyr) - np.argmax(correlation) - 1
-        return diff
+    # @staticmethod
+    # def sync_with_vicon(vicon_gyr, sensor_gyr, check=False):
+    #     correlation = np.correlate(vicon_gyr, sensor_gyr, 'valid')
+    #     vicon_data_len = len(vicon_gyr)
+    #     sensor_data_len = len(sensor_gyr)
+    #     # normalize correlation data
+    #     for i_sample in range(sensor_data_len - vicon_data_len + 1):
+    #         norm_range = range(i_sample, i_sample+vicon_data_len)
+    #         correlation[i_sample] = correlation[i_sample] / norm(sensor_gyr[norm_range])
+    #
+    #     if check:
+    #         plt.plot(correlation)
+    #         print(np.argmax(correlation) + vicon_data_len)
+    #         # plt.show()
+    #     diff = len(sensor_gyr) - np.argmax(correlation) - 1
+    #     return diff
 
